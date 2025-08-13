027c5c34b2bcfa3f1362824f594bdfe4bb600920
Denzil Moses <lawrenmoses@gmail.com>
2025-08-13 18:55:17 +0530

    Create Assignment 2.1.c

---
+++ Assignment 2.1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    // Reverse using loop and swapping
+    for (int i = 0; i < n / 2; i++) {
+        int temp = arr[i];
+        arr[i] = arr[n - 1 - i];
+        arr[n - 1 - i] = temp;
+    }
+
+    // Output
+    printf("Reversed array:\n");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
